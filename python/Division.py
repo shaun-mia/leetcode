56832d8faf4727574128c54e9e01aa01d28c73c3
Shaun Mia <84870531+shaun-mia@users.noreply.github.com>
2025-05-20 23:55:10 +0600

    Add initial Python solutions for various problems including arithmetic operations, division, and conditional statements

---
+++ python/Division.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    int_result=int(a/b)
+    float_result=(a/b)
+    print(int_result)
+    print(float_result)
+
